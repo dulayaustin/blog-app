3a478469ed50869bd718cf6eb9cd029dba26dbd7
Austin Dulay <dulayaustin@gmail.com>
2022-06-26 23:39:03 +0800

    Refactor react LoginLogoutLinks

---
+++ app/javascript/components/Navigation/LoginLogoutLinks.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import isLoggedIn from '../../helpers/isLoggedIn'
+
+const LoginLogoutLinks = () => {
+
+  return (
+    <>
+      {
+        isLoggedIn() ?
+          <Nav.Link className="px-lg-3 py-3 py-lg-4" href="/logout">Logout</Nav.Link>
+          :
+          <Nav.Link className="px-lg-3 py-3 py-lg-4" href="/login">Login</Nav.Link>
+      }
+    </>
+  )
+}
+
+export default LoginLogoutLinks
