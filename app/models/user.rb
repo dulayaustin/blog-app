fb6b5d1284455deb609a726a0be43bc6f4ef0318
Austin Dulay <dulayaustin@gmail.com>
2022-06-27 01:04:41 +0800

    Setup Sign Up page, add first_name last_name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
                 :jwt_authenticatable,
                 :registerable,
                 jwt_revocation_strategy: JwtDenylist
+
+  validates :email, presence: true
 end
