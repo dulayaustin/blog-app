a5a22e34884f88f3d1747fc5fbf46e967a032a38
Austin Dulay <dulayaustin@gmail.com>
2022-07-11 22:25:52 +0800

    Refactor post form to useForm

--- app/javascript/components/Posts/EditPost.js
+++ app/javascript/components/Posts/EditPost.js
@@ -1,8 +1,15 @@
 import { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
-import PostForm from './PostForm'
+import { useParams, useNavigate } from 'react-router-dom'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import Header from '../Header'
+import PostForm from './PostForm'
+
+const schema = yup.object().shape({
+  title: yup.string().required('Title is required'),
+})
 
 function EditPost() {
   const [post, setPost] = useState({})
@@ -20,32 +27,39 @@ function EditPost() {
       .catch((resp) => console.log(resp))
   }, [params.id])
 
-  const handleChange = (e) => {
-    e.preventDefault()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  })
 
-    setPost({ ...post, [e.target.name]: e.target.value })
-  }
+  const navigate = useNavigate()
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
+  const onSubmit = (data) => {
     axios
-      .put(`/api/v1/posts/${params.id}`, { post })
-      .then(() => {
-        window.location = `/posts/${params.id}`
+      .put(`/api/v1/posts/${params.id}`, data)
+      .then((resp) => {
+        navigate(`/posts/${resp.data.data.id}`)
       })
       .catch((resp) => console.log(resp))
   }
+
+  setValue('title', post.title)
+  setValue('content', post.content)
 
   return (
     <>
       <Header title="Edit Post" subtitle="" />
       {loaded && (
         <PostForm
-          title={post.title}
-          content={post.content}
-          handleChange={handleChange}
+          register={register}
           handleSubmit={handleSubmit}
+          onSubmit={onSubmit}
+          errors={errors}
         />
       )}
     </>
